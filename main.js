4baab11674c600fc3b50a4fb5308b1fdf8b8e0d9
ruggs <visthemasteranon@gmail.com>
2024-09-15 18:44:55 -0500

    zzzz

--- main.js
+++ main.js
@@ -13,31 +13,34 @@ textInput.addEventListener("keypress", function (event) {
   }
 });
 
+function cleanString(string) {
+  string = textInput.value.toLowerCase().replace(/[^a-z0-9]/g, "");
+  return string;
+}
+
+function reverseString(string) {
+  let reversedString = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    reversedString += string[i];
+  }
+  return reversedString;
+}
+
 // Checks if any input is a palindrome
 
-function isPalindrome() {
-  console.time("execution time");
+function isPalindrome(palindrome) {
+  palindrome = textInput.value;
+  const input = cleanString(palindrome);
+  const reversedString = reverseString(input);
 
   if (textInput.value === "") {
     alert("Please input a value");
-    console.timeEnd("execution time");
     return;
   }
 
-  const input = textInput.value;
-  const input1 = textInput.value
-    .toLowerCase()
-    .split("")
-    .reverse()
-    .join()
-    .replace(/[^a-z0-9]/g, "");
-  const input2 = textInput.value.toLowerCase().replace(/[^a-z0-9]/g, "");
-
-  if (input1 === input2) {
-    result.innerHTML = `<strong>${input}</strong> is a palindrome`;
+  if (input === reversedString) {
+    result.innerHTML = `<strong>${palindrome}</strong> is a palindrome`;
   } else {
-    result.innerHTML = `<strong>${input}</strong> is not a palindrome`;
+    result.innerHTML = `<strong>${palindrome}</strong> is not a palindrome`;
   }
-
-  console.timeEnd("execution time");
 }
